eb3955f88209110b48204e55ce12910f4a6363b3
Ashraf Uddin <asadbdctg@gmail.com>
2025-11-14 18:11:27 -0500

    CB-V-03

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,70 +4,19 @@ const urlsToCache = [
   '/index.html',
   '/static/js/bundle.js',
   '/static/js/main.chunk.js',
-  '/static/js/0.chunk.js',
-  '/manifest.json'
+  '/static/js/vendors~main.chunk.js'
 ];
 
-// Install event - cache resources
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-      .catch((err) => {
-        console.log('Cache install failed:', err);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-// Fetch event - serve from cache, fallback to network
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then(
-          (response) => {
-            // Check if valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            // Clone the response
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          }
-        );
-      })
-      .catch(() => {
-        // If both cache and network fail, show offline page
-        return caches.match('/index.html');
-      })
+      .then(response => response || fetch(event.request))
   );
-});
-
-// Activate event - clean up old caches
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
 });
